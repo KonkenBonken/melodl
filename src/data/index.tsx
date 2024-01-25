9d951c9cef3164f1f520deab448b73401d25b51e
KonkenBonken <konrad05pettersson@gmail.com>
2024-01-25 23:03:09 +0100

    don't render empty shares

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -55,7 +55,7 @@ class Song {
 
   get Shares() {
     return (() => <div>
-      {this.shares.map((share, i) => (<div key={i}
+      {this.shares.map((share, i) => share > 0 && (<div key={i}
         style={{
           width: (share * 100).toFixed(3) + '%'
         }}
