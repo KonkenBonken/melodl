c38b383de19087d1d385b5584b3dfe8e6aa4e722
KonkenBonken <konrad05pettersson@gmail.com>
2024-01-25 22:28:02 +0100

    choose random song

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import scss from './styles/_main.module.scss';
 
 import data from './data';
 
+const { floor, random } = Math;
+
 export default function Main() {
-  console.log(data);
+  const Goal = useMemo(() => data[floor(random() * data.length)], []);
+  console.log(Goal);
 
   return (
     <h1 className={scss.header}>
