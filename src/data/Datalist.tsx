9bc7ab865201314ac3f5b4518e58398b7ac4d67d
KonkenBonken <konrad05pettersson@gmail.com>
2024-01-27 18:44:15 +0100

    only suggest top 4 closest songs

--- src/data/Datalist.tsx
+++ src/data/Datalist.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { distance } from 'fastest-levenshtein';
 
 import raw from './dataset';
 
-export default function Datalist() {
+export default function Datalist({ input }: { input: string }) {
   return <datalist id="songs">
-    {raw.map(([name]) => (
-      <option value={name} key={name} />
-    ))}
+    {!input || raw
+      .sort((a, b) => distance(input, a[0].substring(0, input.length)) - distance(input, b[0].substring(0, input.length)))
+      .slice(0, 4)
+      .map(([name]) => (
+        <option value={name} key={name} />
+      ))}
   </datalist>;
 }
