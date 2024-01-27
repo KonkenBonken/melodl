c38208774db9a804310b02cf57865cb34a3cd6d8
KonkenBonken <konrad05pettersson@gmail.com>
2024-01-27 16:50:12 +0100

    style guessing

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import useArrayState from 'use-array-state'
+import useArrayState from 'use-array-state';
 
 import scss from './styles/_main.module.scss';
 
@@ -19,7 +19,12 @@ export default function Main() {
       <Goal.Shares />
     </div>
     <div className={scss.guesses}>
-      {guesses.map(guess => guess.Shares())}
+      {guesses.map(Guess => (
+        <div key={Guess.name}>
+          <h3>{Guess.name} <i>{Guess.totalPoints} points</i></h3>
+          <Guess.Shares />
+        </div>
+      ))}
       <input onKeyDown={(e) => {
         if (e.key === 'Enter') {
           const guess = e.currentTarget.value,
